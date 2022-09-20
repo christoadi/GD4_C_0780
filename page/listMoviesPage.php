14d4a4e1ad051b042d866a9379686ebb40cb39f4
christoadi <chrizto.adi456@gmail.com>
2022-09-20 20:51:25 +0700

    update

--- page/listMoviesPage.php
+++ page/listMoviesPage.php
@@ -7,11 +7,11 @@ solid #D40013; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 
 
     <div class="body d-flex justify-content-between">
         <h4>LIST MOVIE</h4>
+        <div class="content-menu ">
+            <a href="./createMoviesPage.php" style="color:red" class="fa fa-plus-square"></a>
+        </div>
     </div>
-    <div class="content-menu ">
-        <i href="./createMoviesPage.php" style="color:red" class="fa fa-plus-square"></i>
-        <a href="./createMoviesPage.php" style="color:black" style="font-weight:800">Create Movies</a>
-    </div>
+
     <hr>
     <table class="table ">
         <thead>
