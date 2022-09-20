aafc2503f250f614eff34fbd248d49ef76aa26bd
christoadi <chrizto.adi456@gmail.com>
2022-09-20 17:08:15 +0700

    update

--- process/deleteMoviesProcess.php
+++ process/deleteMoviesProcess.php
@@ -1,24 +1,23 @@
 <?php
 session_start();
 if(isset($_GET['id'])){
-include ('../db.php');
-$id = $_GET['id'];
-$queryDelete = mysqli_query($con, "DELETE FROM movies WHERE id='$id'") or
-die(mysqli_error($con));
-if($queryDelete){
-echo
-'<script>
-alert("Delete Success"); window.location = "../page/listMoviesPage.php"
+    include ('../db.php');
+    $id = $_GET['id'];
+    $queryDelete = mysqli_query($con, "DELETE FROM movies WHERE id='$id'") or die(mysqli_error($con));
+     if($queryDelete){
+        echo
+        '<script>
+    alert("Delete Success"); window.location = "../page/listMoviesPage.php"
 </script>';
-}else{
-echo
-'<script>
-alert("Delete Failed"); window.location = "../page/listMoviesPage.php"
+    }else{
+        echo
+        '<script>
+    alert("Delete Failed"); window.location = "../page/listMoviesPage.php"
 </script>';
-}
+    }
 }else {
-echo
-'<script>
+    echo
+    '<script>
 window.history.back()
 </script>';
 }
