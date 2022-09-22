fa415c84920578adeb458f683e2c093f42646a70
christoadi <chrizto.adi456@gmail.com>
2022-09-22 18:02:27 +0700

    UGD4_C_10780

--- page/listSeriesPage.php
+++ page/listSeriesPage.php
@@ -7,7 +7,7 @@ solid #D40013; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 
 0.19);">
 
     <div class="body d-flex justify-content-between">
-        <h4>LIST MOVIE</h4>
+        <h4>LIST SERIES</h4>
         <div class="content-menu ">
             <a href="./createSeriesPage.php" style="color:red" class="fa fa-plus-square"></a>
         </div>
