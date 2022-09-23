f0ec0d45cc49e6d7209d083d1622236a13d77f07
christoadi <chrizto.adi456@gmail.com>
2022-09-23 16:25:44 +0700

    TGS4_C_10780

--- process/editSeriesProcess.php
+++ process/editSeriesProcess.php
@@ -39,32 +39,3 @@ if (isset($_POST['edit'])) {
  window.history.back()
  </script>';
 }
-
-function showSelectedSerie($id)
-{
-    include '../db.php';
-    $query = "SELECT * FROM series WHERE id='$id'";
-    $result = mysqli_query($con, $query);
-
-    $row = mysqli_fetch_assoc($result);
-
-    return $row;
-}
-
-function editData($data, $id)
-{
-    include '../db.php';
-
-    $name = $_POST["name"];
-    $genre = implode(", ", $_POST["genre"]);
-    $realese = $data["realese"];
-    $episode = $data["episode"];
-    $season = $data["season"];
-    $synopsis = $data["synopsis"];
-
-    $query = "UPDATE series SET name='$name', genre='$genre', realese='$realese', season='$season', synopsis='$synopsis', episode='$episode' WHERE ID= '$id'";
-
-    $result = mysqli_query($con, $query);
-
-    return mysqli_affected_rows($con);
-}
